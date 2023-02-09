6d8d1eddebf1ce7fe5205e0da6c1e64de13c139d
Daniel Rashba <danirdd92@gmail.com>
2023-02-09 18:42:40 +0200

    ui fixes everywhere

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+      screens: {
+        xs: '380px'
+      }
+    },
   },
   daisyui: {
     logs: false,
